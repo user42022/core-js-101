5c4902db8f1d791296020041344e747efe1bdf8a
user42022 <andrekizilov@yandex.ru>
2023-06-23 00:11:50 +0300

    feat: implement functions in 05-objets-tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,10 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => this.height * this.width;
 }
 
 
@@ -35,8 +37,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +53,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+  return obj;
 }
 
 
@@ -74,7 +78,7 @@ function fromJSON(/* proto, json */) {
  * according to css specification.
  *
  * Provided cssSelectorBuilder should be used as facade only to create your own classes,
- * for example the first method of cssSelectorBuilder can be like this:
+ * for example the first method of cssSelectorBuilder can be like resObj:
  *   element: function(value) {
  *       return new MySuperBaseElementSelector(...)...
  *   },
@@ -111,32 +115,104 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.modificator = this.modificator;
+    if (resObj.modificator) {
+      if (resObj.modificator.slice(1).some((e) => e > 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    resObj.modificator = [1, 0, 0, 0, 0, 0];
+    resObj.value = this.value ? this.value + value : value;
+    return resObj;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.modificator = this.modificator;
+    if (resObj.modificator) {
+      if (resObj.modificator.slice(2).some((e) => e > 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+      if (resObj.modificator[1] > 0) {
+        throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+      }
+      resObj.modificator[1] += 1;
+    }
+    resObj.modificator = !resObj.modificator ? [0, 1, 0, 0, 0, 0] : resObj.modificator;
+    resObj.value = this.value ? `${this.value}#${value}` : `#${value}`;
+    return resObj;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.modificator = this.modificator;
+    if (resObj.modificator) {
+      if (resObj.modificator.slice(3).some((e) => e > 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+      resObj.modificator[2] += 1;
+    }
+    resObj.modificator = !resObj.modificator ? [0, 0, 1, 0, 0, 0] : resObj.modificator;
+    resObj.value = this.value ? `${this.value}.${value}` : `.${value}`;
+    return resObj;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.modificator = this.modificator;
+    if (resObj.modificator) {
+      if (resObj.modificator.slice(4).some((e) => e > 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+      resObj.modificator[3] += 1;
+    }
+    resObj.modificator = !resObj.modificator ? [0, 0, 0, 1, 0, 0] : resObj.modificator;
+    resObj.value = this.value ? `${this.value}[${value}]` : `[${value}]`;
+    return resObj;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.modificator = this.modificator;
+    if (resObj.modificator) {
+      if (resObj.modificator.slice(-1).some((e) => e > 0)) {
+        throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+      }
+      resObj.modificator[4] += 1;
+    }
+    resObj.modificator = !resObj.modificator ? [0, 0, 0, 0, 1, 0] : resObj.modificator;
+    resObj.value = this.value ? `${this.value}:${value}` : `:${value}`;
+    return resObj;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.modificator = this.modificator;
+    if (resObj.modificator) {
+      if (resObj.modificator[5] > 0) {
+        throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+      }
+      resObj.modificator[1] += 1;
+    }
+    resObj.modificator = !resObj.modificator ? [0, 0, 0, 0, 0, 1] : resObj.modificator;
+    resObj.value = this.value ? `${this.value}::${value}` : `::${value}`;
+    return resObj;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const resObj = Object.create(cssSelectorBuilder);
+    resObj.stringify = () => resObj.value;
+    resObj.value = `${selector1.value} ${combinator} ${selector2.value}`;
+    return resObj;
   },
 };
 
